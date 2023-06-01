db4d91f7f0494d3861c109dea439f68132b0b9ca
fmi <fmi@pop-os.localdomain>
2023-06-01 22:05:09 +0200

    Added additional lsp hotkeys

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,8 +14,12 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 lsp.on_attach(function(client, buffer)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(buffer, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
