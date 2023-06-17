935480424ad6c48af0f1958b96a5559a13050852
fmilke <fridjofmilke@gmail.com>
2023-06-17 21:36:24 +0200

    Fixed configuration issues

--- lua/fmi/packer.lua
+++ lua/fmi/packer.lua
@@ -12,6 +12,7 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+  
 
   use({
 	  'rose-pine/neovim',
@@ -26,5 +27,29 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+
+
+  use {
+  	  'VonHeikemen/lsp-zero.nvim',
+  	  branch = 'v1.x',
+  	  requires = {
+  		  -- LSP Support
+  		  {'neovim/nvim-lspconfig'},
+  		  {'williamboman/mason.nvim'},
+  		  {'williamboman/mason-lspconfig.nvim'},
+  
+  		  -- Autocompletion
+  		  {'hrsh7th/nvim-cmp'},
+  		  {'hrsh7th/cmp-buffer'},
+  		  {'hrsh7th/cmp-path'},
+  		  {'saadparwaiz1/cmp_luasnip'},
+  		  {'hrsh7th/cmp-nvim-lsp'},
+  		  {'hrsh7th/cmp-nvim-lua'},
+  
+  		  -- Snippets
+  		  {'L3MON4D3/LuaSnip'},
+  		  {'rafamadriz/friendly-snippets'},
+  	  }
+    }
 end)
 
