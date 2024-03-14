44d26402e5e1fb0ae48dd4c3af1e8cbf1c8e343c
fmilke <fridjofmilke@gmail.com>
2024-03-14 08:23:21 +0100

    added harpoon

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,7 +1,9 @@
 
 local telescope_builtin = require("telescope.builtin")
 
+vim.keymap.set("n", "<C-L>", ":noh<CR>", {})
 vim.keymap.set("n", "<leader>pf", telescope_builtin.find_files, {})
 vim.keymap.set("n", "<leader>ps", function()
     telescope_builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, {})
+
