f231e423581e678875c98f678b0dd76ab3e6fdd4
fmilke <fridjofmilke@gmail.com>
2024-04-12 20:51:47 +0200

    grep search now does nothing on empty string

--- lua/keys.lua
+++ lua/keys.lua
@@ -4,6 +4,9 @@ local telescope_builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-L>", ":noh<CR>", {})
 vim.keymap.set("n", "<leader>pf", telescope_builtin.find_files, {})
 vim.keymap.set("n", "<leader>ps", function()
-    telescope_builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    local needle = vim.fn.input("Grep > ")
+    if not(needle == nil or needle == "") then
+        telescope_builtin.grep_string({ search = needle })
+    end
 end, {})
 
