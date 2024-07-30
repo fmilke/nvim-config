cf6d2d8bf1fa81626161791b0826c416c779c8d6
Fridjof Milke <fridjof.milke@designa.com>
2024-07-30 13:59:43 +0200

    Added basic custom change casing key hotkey

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+P = function(v)
+    print(vim.inspect(v))
+    return v
+end
+
 require("options")
 require("plugins")
 require("keys")
--- lua/keys.lua
+++ lua/keys.lua
@@ -1,6 +1,6 @@
-
 local telescope_builtin = require("telescope.builtin")
 
+vim.keymap.set("n", "<leader>x", ":source %<CR>", {})
 vim.keymap.set("n", "<C-L>", ":noh<CR>", {})
 vim.keymap.set("n", "<leader>pf", telescope_builtin.find_files, {})
 vim.keymap.set("n", "<leader>ps", function()
@@ -13,3 +13,5 @@ end, {})
 vim.keymap.set("n", "<C-d>", function()
     vim.diagnostic.open_float()
 end, { desc = "show line diagnostics" })
+
+vim.keymap.set('x', "<leader>cc", require'change_case'.change_casing, { desc = "change casing" })
